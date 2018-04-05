7e0f5964b6cbdf1151b7baadadc9bdf314899552
Alfie Darko <me@alfiedarko.co.uk>
2018-04-05 14:27:20 +0100

    failing test for NotelistView.returnHTML() ammended expected html string

--- alfspec/note-list-view___spec.js
+++ alfspec/note-list-view___spec.js
@@ -2,12 +2,14 @@ describe('>Notelist View', function() {
   it('>>has a function that references to the instantiated notelist ', function() {
     nlview = new NoteListView()
 
-    assert.doesFunctionExist(nlview, this.NoteList)
+    assert.doesFunctionExist(nlview, this.noteList)
   });
 
   it('>returns a HTML string representing the notelist', function() {
     nlview = new NoteListView();
-
-    assert.nlview.returnHTML()
+    nlview.notelist.createNote('hello')
+    nlview.notelist.createNote('goodbye')
+    var htmlString = "<ul><li><div>hello</div></li><li><div>goodbye</div></li></ul>"
+    assert.isEqual(nlview.returnHTML(), htmlString)
   });
 });
