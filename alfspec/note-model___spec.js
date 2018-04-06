b2b4c2f9cf1efed4c19dadfab135f0b93e358f06
Alfie Darko <me@alfiedarko.co.uk>
2018-04-06 21:44:10 +0100

    failing test for NoteModel > unique serial autogenerating id

--- alfspec/note-model___spec.js
+++ alfspec/note-model___spec.js
@@ -17,6 +17,9 @@ describe('Note Model', function() {
   });
 
   it('    the serial ids are unique and generate automatically', function() {
+    noteText = "new note"
+    note = new Note(noteText)
+    assert.isEqual(note._id, 1)
 
   });
 });
