0d4dfe1581221206e930c4de53e27a7ad70366c3
Alfie Darko <me@alfiedarko.co.uk>
2018-04-06 16:36:19 +0100

    added passing test for notelistview method to truncate strings to 20 chars

--- alfspec/note-list-view___spec.js
+++ alfspec/note-list-view___spec.js
@@ -12,4 +12,12 @@ describe('Notelist View', function() {
     var htmlString = "<ul><li><div>hello</div></li><li><div>goodbye</div></li></ul>"
     assert.isEqual(nlview.returnHTML(), htmlString)
   });
+
+  it('    should only display the first 20 characters of a note', function() {
+    nlview = new NoteListView(new NoteList());
+    nlview.notelist.createNote('Shopping list: Bananas, bread, egg, meat, milk, honey. Top up electric with £20')
+    var htmlString = "<ul><li><div>Shopping list:...</div></li></ul>"
+    assert.isEqual(nlview.returnHTML(), htmlString)
+
+  });
 });
--- note-list-view.js
+++ note-list-view.js
@@ -3,10 +3,21 @@
     this.notelist = notelist
   }
 
+  String.prototype.trunc =
+    function(n, useWordBoundary) {
+      if (this.length <= n) {
+        return this;
+      }
+      var subString = this.substr(0, n - 1);
+      return (useWordBoundary ?
+        subString.substr(0, subString.lastIndexOf(' ')) :
+        subString) + "...";
+    };
+
   NoteListView.prototype.returnHTML = function() {
     var htmlString = "<ul>" + this.notelist.notes.map(
       note =>
-      `<li><div>${note.text}</div></li>`).join("") + "</ul>"
+      `<li><div>${note.text.trunc(20, true)}</div></li>`).join("") + "</ul>"
     return htmlString
   };
 
